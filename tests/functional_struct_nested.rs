958ca8276e33a8050b99b022e39d07a08229ea3a
EricLBuehler <ericlbuehler@gmail.com>
2024-06-19 02:36:43 -0400

    Add more tests

---
+++ tests/functional_struct_nested.rs
@@ -0,0 +1,17 @@
+use pyo3::pyclass;
+use pyo3_special_method_derive::{Dir, Repr, Str};
+
+#[pyclass]
+#[derive(Dir, Str, Repr)]
+#[allow(dead_code)]
+struct B {
+    pub z: u32,
+}
+
+#[pyclass]
+#[derive(Dir, Str, Repr)]
+#[allow(dead_code)]
+struct A {
+    pub x: u32,
+    pub y: Option<B>,
+}
