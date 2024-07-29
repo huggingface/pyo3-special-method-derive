527d6bcbd16f969159b18bef511f43f2d82b6d25
Arthur Zucker <arthur.zucker@gmail.com>
2024-07-29 17:46:07 +0200

    almost thre

--- pyo3_special_method_derive_macro_0_21/src/str_repr.rs
+++ pyo3_special_method_derive_macro_0_21/src/str_repr.rs
@@ -221,7 +221,7 @@ fn generate_fmt_impl_for_enum(
                                 }).collect();
                             quote! {
                                 // For each arm format is gonna be hard to specify no?
-                                Self::#variant_name {#(#ids,)*} => repr += &format!(#(#format_strings),* #(#token_streams,)*),
+                                Self::#variant_name {#(#ids,)*} => repr += &format!(#(#format_strings)* #(#token_streams)*),
                             }
                         };
                         Ok(field_arm)
